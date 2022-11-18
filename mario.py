21bc063478b743fd5cb7e80762daa7d9fe60e3f3
Vlademir Capelin Junior <112670199+VlademirCapelinJunior@users.noreply.github.com>
2022-11-18 18:39:18 -0300

    Update mario.py

--- mario.py
+++ mario.py
@@ -1,21 +1,53 @@
 import pygame
-import random
+import os
 import time
+import random
+
 pygame.init()
-pygame_display = pygame.display
-pygame_display.set_caption('Super Mario')
-altura = 0
-largura = 0
-tamanho = (altura, largura)
-game_display = pygame.display.set_mode(tamanho)
-clock = pygame.time.Clock()
-game_events = pygame.event
-branco = (255,255,255)
-fundo = pygame.image.load('assets/fundo.jpg')
 
-def escrever_texto(texto):
-    fonte = pygame.font.Font('freesansbold.ttf', 15)
-    texto_display = fonte.render(texto,True,branco)
-    game_display.blit(texto_display, (880,80))
-    pygame_display.update()
 
+def limparTela():
+    os.system("cls")
+
+
+limparTela()
+print("Jogo do mario")
+
+largura = 600
+altura = 428
+tamanho = (largura, altura)
+branco = (255, 255, 255)
+display = pygame.display.set_mode(tamanho)
+fps = pygame.time.Clock()
+gameEvents = pygame.event
+
+mario = pygame.image.load("assets/mario.png")
+fundo = pygame.image.load("assets/fundo teste.jpg")
+pygame.display.set_caption("SUPER MARIO DOS GURI")
+
+jogando = True
+marioX = 400
+marioY = 400
+marioAltura = 93
+marioLargura = 93
+movimentoMarioX = 0
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                movimentoMarioX = -15
+            elif evento.key == pygame.K_RIGHT:
+                movimentoMarioX = 15
+            elif evento.type == pygame.KEYUP:
+                movimentoMarioX = 0
+        display.fill(branco)
+        posicao = (movimentoMarioX, 100)
+        display.blit(fundo, (0, 0))
+        display.blit(mario, posicao)
+
+        pygame.display.update()
+        fps.tick(144)
